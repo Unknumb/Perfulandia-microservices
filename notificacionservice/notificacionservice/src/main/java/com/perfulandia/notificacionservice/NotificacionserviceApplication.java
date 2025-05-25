95f62c20beab5ef3a148116999a37489b6d7117a
Juan Ignacio Toledo <ju.toledod@duocuc.cl>
2025-05-25 17:51:09 -0400

    Creacion spring NotificacionService

---
+++ notificacionservice/notificacionservice/src/main/java/com/perfulandia/notificacionservice/NotificacionserviceApplication.java
@@ -0,0 +1,13 @@
+package com.perfulandia.notificacionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificacionserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificacionserviceApplication.class, args);
+	}
+
+}
