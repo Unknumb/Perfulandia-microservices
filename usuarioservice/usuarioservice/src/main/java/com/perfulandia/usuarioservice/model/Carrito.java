54c7380a5a15b52015f5e040f362c45953ba3d76
Unknumb <theguzzyboy@gmail.com>
2025-05-27 14:37:31 -0400

    "avance conexion microservicio"

--- usuarioservice/usuarioservice/src/main/java/com/perfulandia/usuarioservice/model/Carrito.java
+++ usuarioservice/usuarioservice/src/main/java/com/perfulandia/usuarioservice/model/Carrito.java
@@ -1,24 +1,24 @@
-package com.perfulandia.carritoservice.model;
+package com.perfulandia.usuarioservice.model;
 
-import jakarta.persistence.*;
+
+import jakarta.persistence.ElementCollection;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import lombok.*;
+
 import java.util.List;
 
-@Entity
+//dto
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 public class Carrito {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-
     private long usuarioId;
-
-    @ElementCollection
     private List<Long> productoIds;
-
     private Double total;
+
 }
