58be4ba9e2864edf374839a4424edf9491081f2a
99nicolas <nicolas.holck25@gmail.com>
2025-05-29 22:18:17 -0400

    modificacion de controller para notificacion

--- carritoservice/carritoservice/src/main/java/com/perfulandia/carritoservice/controller/CarritoController.java
+++ carritoservice/carritoservice/src/main/java/com/perfulandia/carritoservice/controller/CarritoController.java
@@ -1,8 +1,10 @@
 package com.perfulandia.carritoservice.controller;
 
 import com.perfulandia.carritoservice.model.Carrito;
+import com.perfulandia.carritoservice.model.Notificacion;
 import com.perfulandia.carritoservice.service.CarritoService;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
@@ -11,9 +13,11 @@ import java.util.List;
 public class CarritoController {
 
     private final CarritoService carritoService;
+    private final RestTemplate restTemplate;
 
-    public CarritoController(CarritoService carritoService) {
+    public CarritoController(CarritoService carritoService, RestTemplate restTemplate) {
         this.carritoService = carritoService;
+        this.restTemplate = restTemplate;
     }
 
     @GetMapping
@@ -28,7 +32,13 @@ public class CarritoController {
 
     @PostMapping
     public Carrito guardar(@RequestBody Carrito carrito) {
-        return carritoService.guardar(carrito);
+        Carrito savedCarrito = carritoService.guardar(carrito);
+
+        String mensaje = "Se creó tu carrito #" + savedCarrito.getId() + " con total $" + savedCarrito.getTotal();
+        Notificacion notificacion = new Notificacion(savedCarrito.getUsuarioId(), mensaje);
+        restTemplate.postForObject("http://localhost:8084/api/notificaciones", notificacion, Notificacion.class);
+
+        return savedCarrito;
     }
 
     @DeleteMapping("/{id}")
