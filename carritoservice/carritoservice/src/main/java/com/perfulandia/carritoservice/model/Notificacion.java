5e09745ac80e1782d720e4ac3e2721e61101dfc6
99nicolas <nicolas.holck25@gmail.com>
2025-05-29 22:15:24 -0400

    Creacion de notificacion para carrito

---
+++ carritoservice/carritoservice/src/main/java/com/perfulandia/carritoservice/model/Notificacion.java
@@ -0,0 +1,13 @@
+package com.perfulandia.carritoservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Notificacion {
+    private long usuarioId;
+    private String mensaje;
+}
