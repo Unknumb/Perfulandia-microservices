b1d8dfd4c422d64350691053ba51613278878a65
Juan Ignacio Toledo <ju.toledod@duocuc.cl>
2025-05-29 23:51:09 -0400

    Desarrollo de codigo para ver el carrito en notificacion

--- notificacionservice/notificacionservice/src/main/java/com/perfulandia/notificacionservice/NotificacionserviceApplication.java
+++ notificacionservice/notificacionservice/src/main/java/com/perfulandia/notificacionservice/NotificacionserviceApplication.java
@@ -2,11 +2,18 @@ package com.perfulandia.notificacionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class NotificacionserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NotificacionserviceApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 }
--- notificacionservice/notificacionservice/src/main/java/com/perfulandia/notificacionservice/controller/NotificacionController.java
+++ notificacionservice/notificacionservice/src/main/java/com/perfulandia/notificacionservice/controller/NotificacionController.java
@@ -1,19 +1,25 @@
 package com.perfulandia.notificacionservice.controller;
 
+import com.perfulandia.notificacionservice.model.Carrito;
 import com.perfulandia.notificacionservice.model.Notificacion;
 import com.perfulandia.notificacionservice.service.NotificacionService;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
-@RequestMapping("api/notificaciones")
+@RequestMapping("/notificaciones")
 public class NotificacionController {
 
     private final NotificacionService notificacionService;
+    private final RestTemplate restTemplate;
 
-    public NotificacionController(NotificacionService notificacionService) {
+    public NotificacionController(NotificacionService notificacionService, RestTemplate restTemplate) {
         this.notificacionService = notificacionService;
+        this.restTemplate = restTemplate;
     }
 
     @GetMapping
@@ -34,5 +40,18 @@ public class NotificacionController {
     @DeleteMapping("/{id}")
     public void eliminar(@PathVariable Long id) {
         notificacionService.eliminar(id);
+    }
+
+    @GetMapping("/detallada/{id}")
+    public Map<String, Object> obtenerNotificacionConCarrito(@PathVariable Long id) {
+        Notificacion notificacion = notificacionService.obtenerPorId(id);
+        Carrito carrito = restTemplate.getForObject(
+                "http://localhost:8083/api/carritos/" + notificacion.getCarritoId(),
+                Carrito.class
+        );
+        Map<String, Object> respuesta = new HashMap<>();
+        respuesta.put("notificacion", notificacion);
+        respuesta.put("carrito", carrito);
+        return respuesta;
     }
 }
